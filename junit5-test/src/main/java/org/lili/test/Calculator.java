f6a5cfeb97a5f30fbe5e465097a38b328aaec6e4
lili <xiaozhiliaoo@gmail.com>
2020-08-07 12:24:37 +0800

    add junit5

---
+++ junit5-test/src/main/java/org/lili/test/Calculator.java
@@ -0,0 +1,12 @@
+package org.lili.test;
+
+/**
+ * @author lili
+ * @date 2020/8/7 12:22
+ * @notes
+ */
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
