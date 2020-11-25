cef805c13c359c2eb1dca5cd48b52224d703e25b
lili <xiaozhiliaoo@gmail.com>
2020-11-25 23:21:28 +0800

    up

---
+++ assertj/src/main/java/org/lili/assertj/Main.java
@@ -0,0 +1,11 @@
+package org.lili.assertj;
+
+/**
+ * @author lili
+ * @date 2020/11/25 23:18
+ * @notes
+ */
+public class Main {
+
+
+}
---
+++ assertj/src/test/java/org/lili/assertj/MainTest.java
@@ -0,0 +1,20 @@
+package org.lili.assertj;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+/**
+ * @author lili
+ * @date 2020/11/25 23:20
+ * @notes
+ */
+class MainTest {
+
+    @Test
+    public void test() {
+
+    }
+
+}
